67f59f849169bc656a5f07cafde66f37fee28da3
rainboowbee <rainboowbee@gmail.com>
2025-02-25 18:15:42 +0300

    админка

---
+++ pages/admin.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      <p>инструменты для управления платформой</p>
+    </div>
+  );
+};
+
+export default AdminPage;
